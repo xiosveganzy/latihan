c97f04ed1f88b0267ea257acdf6cdebb3bd2cb37
angkatan24 <42346081+npmbots@users.noreply.github.com>
2019-07-28 15:45:57 +0700

    Update Main.py

--- 1/Main.py
+++ 1/Main.py
@@ -1,1 +1,17 @@
+# latihan 1 : tentang os
 
+import os
+
+
+# variable
+
+hans = "sayang"
+
+# Console Print
+
+print hans
+print ""
+os.system('clear')
+os.system('pkg install figlet')
+os.system('clear')
+os.system('figlet SEKIAN')
